ce694985773bbcee4d5f1c5a0ef9e6f8674e2793
lubinbenoit <lubin.benoit@gmail.com>
2026-01-13 13:09:35 +0100

    feat: adding initial content to scrapy components

---
+++ fragrantica_scraper/items.py
@@ -0,0 +1,7 @@
+import scrapy
+
+class FragranticaPerfumeItem(scrapy.Item):
+    name = scrapy.Field()
+    brand = scrapy.Field()
+    accords = scrapy.Field()  # dict {accord: pourcentage}
+    url = scrapy.Field()
---
+++ fragrantica_scraper/spiders/spider.py
@@ -0,0 +1,32 @@
+import scrapy
+from fragrantica_scraper.items import FragranticaPerfumeItem
+import re
+
+class FragranticaSpider(scrapy.Spider):
+    name = "fragrantica"
+    allowed_domains = ["fragrantica.com"]
+    start_urls = [
+        "https://www.fragrantica.com/perfume/Chanel/Chanel-No-5-100.html"
+    ]
+
+    def parse(self, response):
+        item = FragranticaPerfumeItem()
+
+        item["url"] = response.url
+        item["name"] = response.css("h1::text").get()
+        item["brand"] = response.css(".perfume-brand::text").get()
+
+        accords = {}
+
+        for accord in response.css(".accord-bar"):
+            name = accord.css(".accord-name::text").get()
+            style = accord.css(".accord-value::attr(style)").get()
+
+            if style:
+                percentage = re.search(r"width:\s*(\d+)%", style)
+                if percentage:
+                    accords[name] = int(percentage.group(1))
+
+        item["accords"] = accords
+
+        yield item
