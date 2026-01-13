0a9771802b55fe06e71fbfca4c31ff8e1d6be26a
LuJavac <tonloplucas@gmail.com>
2026-01-13 14:32:02 +0100

    Ajout de la configuration uv et des fichiers de base par [Lucas TONLOP]

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from fragrantica!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ test_config.py
@@ -0,0 +1,2 @@
+import pandas as pd
+print("Succès : L'environnement Fragrantica est opérationnel !")
